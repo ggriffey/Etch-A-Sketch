a9d88bc5b4e2db7faa89a0f1136b5f036c767aaa
Greyson Griffey <ggriffey@greysons-air.myfiosgateway.com>
2022-09-08 23:34:22 -0400

    Add paint-on-hover and reset button functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const createGrid = (height, length) => {
+// create initial grid:
+const createGrid = (x) => {
   console.log("creating grid!");
   let grid = document.createElement("table");
   grid.className = "grid";
@@ -6,14 +7,14 @@ const createGrid = (height, length) => {
   container.appendChild(grid);
 
   // for each row (height):
-  for (let i = 0; i < height; i++) {
+  for (let i = 0; i < x; i++) {
     console.log("creating row!");
     // create new table row
     let row = document.createElement("tr");
     row.cellName = "row";
 
     // then: for each row, create "length" # of cells
-    for (let j = 0; j < length; j++) {
+    for (let j = 0; j < x; j++) {
       console.log("creating cell!");
       // create and add cell to row
       let cell = document.createElement("td");
@@ -27,10 +28,24 @@ const createGrid = (height, length) => {
   }
 };
 
-createGrid(4, 3);
-/* expected output: 
-    _   _   _
-    _   _   _
-    _   _   _
-    _   _   _
-*/
+createGrid(16);
+
+const cells = document.getElementsByClassName("cell");
+
+// reset button logic
+const button = document.getElementById("reset-button");
+// Querying elements from the DOM returns an HTMLCollection...
+// Thus, we cannot treat the result as an array and use foreach
+// in the usual manner. However, there is THIS workaround:
+button.addEventListener("click", () => {
+  Array.from(cells).forEach((cell) => {
+    cell.classList.remove("fill");
+  });
+});
+
+// listen for mouseover event
+Array.from(cells).forEach((cell) => {
+  cell.addEventListener("mouseover", () => {
+    cell.classList.add("fill");
+  });
+});
