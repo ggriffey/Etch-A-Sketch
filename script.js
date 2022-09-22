4a725bb034d232d5e5d3d1514af3d4f2e70cc6a3
Greyson Griffey <ggriffey@greysons-air.myfiosgateway.com>
2022-09-22 01:35:40 -0400

    Add canvas size functionality; refactor/improve design

--- script.js
+++ script.js
@@ -5,24 +5,32 @@ const BG_DEFAULT = "rgba(211, 203, 192, 0.5)";
 const PAINT_DEFAULT = colorWheel.value;
 let paint_color = PAINT_DEFAULT;
 
-const updateColor = () => {
-  paint_color = colorWheel.value;
-  console.log("color updated");
-};
+const updateColor = () => (paint_color = colorWheel.value);
 
 // ________________ INITIAL GRID __________________
+const container = document.getElementById("container");
 let grid = document.querySelector(".grid");
-let gridSize = 16;
+let defaultSize = 20;
+let gridExists = false;
 
-const createGrid = (x) => {
+const createGrid = (size) => {
+  // remove previous grid (if any)
+  if (gridExists) {
+    container.removeChild(grid);
+    let newGrid = document.createElement("table");
+    grid = newGrid;
+    grid.classList.add("grid");
+    container.appendChild(grid);
+  }
+
   // for each row (height):
-  for (let i = 0; i < x; i++) {
+  for (let i = 0; i < size; i++) {
     // create new table row
     let row = document.createElement("tr");
     row.className = "row";
 
     // for each row, create # of cells
-    for (let j = 0; j < x; j++) {
+    for (let j = 0; j < size; j++) {
       // create & add cell to row
       let cell = document.createElement("td");
       cell.className = "cell";
@@ -32,10 +40,11 @@ const createGrid = (x) => {
     // add the current row to the grid
     grid.appendChild(row);
   }
+  gridExists = true;
+  grid.style.backgroundColor = BG_DEFAULT;
 };
 
-createGrid(gridSize);
-grid.style.backgroundColor = BG_DEFAULT;
+createGrid(defaultSize);
 
 // _____________ LISTEN FOR COLOR UPDATE ________________
 colorWheel.addEventListener("input", updateColor);
@@ -57,15 +66,41 @@ const paint = (cell) => {
   painting ? (cell.style["background-color"] = `${paint_color}`) : false;
 };
 
-// grid listeners:
-grid.addEventListener("mouseleave", () => (painting = false));
-grid.addEventListener("mouseup", () => (painting = false));
+const addGridListeners = () => {
+  // grid listeners:
+  grid.addEventListener("mouseleave", () => (painting = false));
+  grid.addEventListener("mouseup", () => (painting = false));
 
-// cell listeners:
-Array.from(cells).forEach((cell) => {
-  cell.addEventListener("mousedown", () => {
-    painting = true;
-    paint(cell);
+  // cell listeners:
+  Array.from(cells).forEach((cell) => {
+    cell.addEventListener("mousedown", () => {
+      painting = true;
+      paint(cell);
+    });
+    cell.addEventListener("mouseenter", () => paint(cell));
   });
-  cell.addEventListener("mouseenter", () => paint(cell));
+};
+
+addGridListeners();
+
+// ________________ UPDATE GRID __________________
+const GRID_SIZES = [16, 20, 24];
+
+const small = document.getElementById("small");
+const medium = document.getElementById("med");
+const large = document.getElementById("large");
+
+small.addEventListener("click", () => {
+  createGrid(GRID_SIZES[0]);
+  addGridListeners();
+});
+
+medium.addEventListener("click", () => {
+  createGrid(GRID_SIZES[1]);
+  addGridListeners();
+});
+
+large.addEventListener("click", () => {
+  createGrid(GRID_SIZES[2]);
+  addGridListeners();
 });
